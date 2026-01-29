6917748c9a8c7e45e5cfc380359168d2bbd42786
user <akparaliyevikromjon7@gmail.com>
2026-01-29 09:30:36 +0500

    homework started

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+const sidebar = [
+  {
+    icon:'',
+    title:''
+  },
+    {
+    icon:'',
+    title:''
+  },
+  {
+    icon:'',
+    title:''
+  },
+  {
+    icon:'',
+    title:''
+  },
+  {
+    icon:'',
+    title:''
+  },
+  {
+    icon:'',
+    title:''
+  },
+  {
+    icon:'',
+    title:''
+  },
+  {
+    icon:'',
+    title:''
+  },
+
+]
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
