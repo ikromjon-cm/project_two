4468b3a5ddb69620a589bcc78dc7c15951b44000
user <akparaliyevikromjon7@gmail.com>
2026-01-30 17:20:03 +0500

    sidebar finished

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,29 +1,41 @@
 import './Sidebar.css'
-import React from 'react';
+import React, { useState } from 'react'; 
 
 const Sidebar = ({data}) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const none = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className='sidebar'>
+    <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+        
+        <button className="btn" onClick={none}>
+            {isOpen ? '❌' : '✅'}
+        </button>
+
         <h1>Bistro</h1>
         <p>menu</p>
-      {data.map((item,index)=>
-    <div key={ index} className="bar">
-        <nav >
+
+        {data.map((item, index) => (
+            <div key={index} className="bar">
+                <nav>
+                    <ul>
+                        <li>{item.url} <p>{item.title}</p></li>
+                    </ul>
+                </nav>
+            </div>
+        ))}
+
+        <b>other</b>
+        <nav>
             <ul>
-                <li>{item.url} <p>{item.title}</p></li>
+                {data.map((item, index) => (
+                    <li key={index}>{item.icon} <p>{item.subtitle}</p></li>
+                ))}
             </ul>
         </nav>
-    </div>
-    
-    )}
-    <b>other</b>
-    <nav>
-        <ul>
-            {data.map((item,index)=>
-            <li key={index}>{item.icon} <p>{item.subtitle}</p></li>
-            )}
-        </ul>
-    </nav>
     </div>
   );
 }
