61d66301211b9ce485052458a61e0064f618e552
user <akparaliyevikromjon7@gmail.com>
2026-01-30 14:16:34 +0500

    homework error

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ import { PiFlowerLotus } from "react-icons/pi";
 import { LuSquareActivity } from "react-icons/lu";
 import { FaWallet } from "react-icons/fa6";
 import { CiChat1 } from "react-icons/ci";
-const sidebar =
+const sidebar =[
 
-
-{
-  url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRebaZBPICznY8rw4L5_MSv2pQ4swa3L_qF1sq-_CJNqqBbPq6s'
-},
   {
-    icon: [<HiOutlineHome />, <MdOutlineMenuBook />, <FaTableList />, <PiFlowerLotus />, <HiUsers />, <LuSquareActivity />, <FaWallet />, <CiChat1 />],
-title: ['Dashboard', 'Order Line', 'Manage Table', 'Menus', 'Customer', 'Analytics', 'Payment', 'Chat']
-    },
+    url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRebaZBPICznY8rw4L5_MSv2pQ4swa3L_qF1sq-_CJNqqBbPq6s'
+  },
+  {
+    icon: [ <HiOutlineHome/>, <MdOutlineMenuBook />, <FaTableList />, <PiFlowerLotus />, <HiUsers />, <LuSquareActivity />, <FaWallet />, <CiChat1 /> ],
+    title: ['Dashboard', 'Order Line', 'Manage Table', 'Menus', 'Customer', 'Analytics', 'Payment', 'Chat']
+  }
+]
   
     
-]
+
 import React from 'react';
 
 const App = () => {
