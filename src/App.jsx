18dc4db8ada78fbb20a0844c17b440a290912dfc
user <akparaliyevikromjon7@gmail.com>
2026-01-30 14:20:30 +0500

    homework error star

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,4 @@
 import Sidebar from "./components/Sidebar/Sidebar";
-import { HiOutlineHome } from "react-icons/hi2";
-import { FaTableList } from "react-icons/fa6";
-import { MdOutlineMenuBook } from "react-icons/md";
-import { HiUsers } from "react-icons/hi";
-import { PiFlowerLotus } from "react-icons/pi";
-import { LuSquareActivity } from "react-icons/lu";
-import { FaWallet } from "react-icons/fa6";
-import { CiChat1 } from "react-icons/ci";
-const sidebar =[
-
-  {
-    url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRebaZBPICznY8rw4L5_MSv2pQ4swa3L_qF1sq-_CJNqqBbPq6s'
-  },
-  {
-    icon: [ <HiOutlineHome/>, <MdOutlineMenuBook />, <FaTableList />, <PiFlowerLotus />, <HiUsers />, <LuSquareActivity />, <FaWallet />, <CiChat1 /> ],
-    title: ['Dashboard', 'Order Line', 'Manage Table', 'Menus', 'Customer', 'Analytics', 'Payment', 'Chat']
-  }
-]
-  
     
 
 import React from 'react';
@@ -25,7 +6,6 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <Sidebar data={sidebar} />
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,9 @@
 import React from 'react';
 
-const Sidebar = ({data}) => {
+const Sidebar = () => {
   return (
     <div>
-      {data.map((item,index)=>
-    <div key={index} className="sidebar">
-        <div className="logo">
-            <img src={item.url} alt="icon" />
-            <h2>Bistro</h2>
-        </div>
-        <p>menu</p>
-        <nav>
-            <ul>
-                <li>
-                    {item.icon } <span>{item.title}</span>
-                </li>
-                
-            </ul>
-        </nav>
-    </div>
-    )}
+      
     </div>
   );
 }
